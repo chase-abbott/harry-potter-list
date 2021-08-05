9a6f950fe5466e928a2c156ff0a23d5b46364240
Chase <cabbott93@gmail.com>
2021-08-04 17:18:54 -0700

    passing app test

--- src/components/app/App.test.jsx
+++ src/components/app/App.test.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import { render, cleanup } from '@testing-library/react';
+import { MemoryRouter as Router } from 'react-router';
 import App from './App';
 
 describe('App component', () => {
   afterEach(() => cleanup());
-  it('renders App', () => {
-    const { asFragment } = render(<App />);
+  it('renders App', async () => {
+    const { asFragment } = render(
+      <Router>
+        <App />
+      </Router>
+    );
     expect(asFragment()).toMatchSnapshot();
   });
 });
